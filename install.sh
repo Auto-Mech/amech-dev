9053d4e5f0268dc1529c24657b8dee4308b9ca6d
Andreas Copan <avcopan@gmail.com>
2024-05-21 11:04:45 -0400

    New: Adds install script
    This is currently only working for AutoChem

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# This script runs an editable pip install inside each repo, placing their python
+# libraries in the environment path
+#
+# Arguments:
+#   - Command to run (default: git status)
+
+set -e  # if any command fails, quit
+REPOS=("autochem" "nonexistent")
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+# 1. Enter the script directory and start the pixi shell
+(
+    cd ${SCRIPT_DIR}
+
+    # 2. Loop through each repo and execute the install command
+    for repo in ${REPOS[@]}
+    do
+        printf "\n*** Installing in ${SCRIPT_DIR}/src/${repo} ***\n"
+        (
+            cd src/${repo} && \
+            pixi run pip install -e . --no-deps
+        )
+        printf "******\n"
+    done
+)
