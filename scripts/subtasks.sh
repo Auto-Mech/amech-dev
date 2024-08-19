4372c75e27b638df3ce0eac881be34716feadc22
avcopan <avcopan@gmail.com>
2024-08-19 14:31:44 -0500

    New: Set log file output for subtask run script (WIP)

--- scripts/subtasks.sh
+++ scripts/subtasks.sh
@@ -3,11 +3,17 @@
 # Optionally, read in the subtask directory path with a `-p` argment
 # (Must be the first argument)
 SUBTASK_PATH="subtasks"
-while getopts ":p:" opt; do
+SUBTASK_LOG="sub.log"
+ARG_OFFSET=1
+while getopts ":p:o:" opt; do
   case $opt in
     p)
       SUBTASK_PATH=$OPTARG
-      set -- "${@:3}" # remove flag and argument from list of nodes
+      ARG_OFFSET=$((ARG_OFFSET+2))
+      ;;
+    o)
+      SUBTASK_LOG=$OPTARG
+      ARG_OFFSET=$((ARG_OFFSET+2))
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
@@ -21,12 +27,13 @@ while getopts ":p:" opt; do
 done
 
 # Read in the list of nodes
-read -a NODES <<< "${@}"
+read -a NODES <<< "${@:ARG_OFFSET}"
 FIRST_NODE=${NODES[0]}
 NODES_CSV="$(printf '%s,' ${NODES[@]})"
 
 echo "Arguments:"
 echo "  SUBTASK_PATH=${SUBTASK_PATH}"
+echo "  SUBTASK_LOG=${SUBTASK_LOG}"
 echo "  NODES_CSV=${NODES_CSV}"
 
 ACTIVATION_HOOK="$(pixi shell-hook)"
@@ -46,5 +53,5 @@ ssh ${FIRST_NODE} /bin/env bash << EOF
     cd $(pwd)
     ${SCRIPT_HEADER}
     eval ${ACTIVATION_HOOK@Q}
-    nohup sh -c ${SCRIPT@Q} > sub.log 2>&1 &
+    nohup sh -c ${SCRIPT@Q} > ${SUBTASK_LOG} 2>&1 &
 EOF
