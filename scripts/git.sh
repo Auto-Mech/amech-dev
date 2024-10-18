d9b822a03269959959317f139592d73f30739506
avcopan <avcopan@gmail.com>
2024-10-17 20:17:23 -0500

    WIP: Accountability system for local tests
    Instead of archiving the individual save and subtask directories, I need to
    archive the whole test directory **along with** the YAML files.
    
    Also, the archiving should happen when I sign off on the data.

--- scripts/git.sh
+++ scripts/git.sh
@@ -10,19 +10,19 @@ REPOS=("autochem" "autoio" "autofile" "mechanalyzer" "mechdriver")
 DIR=$( dirname -- $( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ))
 
 # 0. Read arguments
-COMMAND="${@:-git status}"
+COMMAND="${@:-status}"
 
 # 1. If running something other than git status, confirm
 if [[ "${COMMAND}" != "git status" ]]; then
-    read -p "Execute '${COMMAND}' in each repository? Press enter to confirm "
+    read -p "Execute 'git ${COMMAND}' in each repository? Press enter to confirm "
 fi
 
 # 2. Loop through each repo and execute the command
 for repo in ${REPOS[@]}
 do
-    printf "\n*** Running command in ${DIR}/src/${repo} ***\n"
+    printf "\n*** Running 'git ${COMMAND}' in ${DIR}/src/${repo} ***\n"
     (
-        cd ${DIR}/src/${repo} && ${COMMAND}
+        cd ${DIR}/src/${repo} && git ${COMMAND}
     )
     printf "******\n"
 done
